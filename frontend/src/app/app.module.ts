82543a6db4c1816f79efc4c7a9dc6ae2d8fdc2ae
washi <washington.parra@gmail.com>
2021-10-18 22:43:43 -0300

    Product model

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,8 +16,8 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -29,7 +29,6 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ForDirective,
     ProductCreateComponent
   ],
   imports: [
@@ -39,7 +38,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
